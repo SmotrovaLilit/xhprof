5e72db6ef9622526711e036ce2e20b83f9f96864
lilit <lilit2990@gmail.com>
2017-09-20 02:15:42 +0300

    правки

--- example.php
+++ example.php
@@ -1,15 +1,16 @@
 <?php
 include_once __DIR__ . '/vendor/autoload.php';
 $hostForDisplayReports = 'http://xhprof-display.dev.lilit-web.ru/xhprof_html';
+$type = 'xhprof-example';
 
 use SmotrovaLilit\ProfileServiceXhprof;
 
-$profile = new ProfileServiceXhprof('test');
+$profile = new ProfileServiceXhprof($type);
 $profile->startProfiling();
 echo "test";
 $reportId = $profile->endProfiling();
 ?>
 <p>
-    <a target="_blank" href="<?= $hostForDisplayReports . '/index.php?run=' . $reportId . '&source=test' ?>">Посмотреть
+    <a target="_blank" href="<?= $hostForDisplayReports . '/index.php?run=' . $reportId . '&source=' . $type ?>">Посмотреть
         отчет</a>
 </p>
