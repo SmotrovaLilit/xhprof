dab6eb3158539256df37aec57a879cafe4d7a7c2
lilit <lilit2990@gmail.com>
2017-09-20 02:04:57 +0300

    ссылка на просмотр отчета

--- example.php
+++ example.php
@@ -1,9 +1,15 @@
 <?php
 include_once __DIR__ . '/vendor/autoload.php';
 $hostForDisplayReports = 'http://xhprof-display.dev.lilit-web.ru/xhprof_html';
+
 use SmotrovaLilit\ProfileServiceXhprof;
 
 $profile = new ProfileServiceXhprof('test');
 $profile->startProfiling();
 echo "test";
 $reportId = $profile->endProfiling();
+?>
+<p>
+    <a target="_blank" href="<?= $hostForDisplayReports . '/index.php?run=' . $reportId . '&source=test' ?>">Посмотреть
+        отчет</a>
+</p>
